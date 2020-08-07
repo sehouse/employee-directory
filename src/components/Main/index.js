d132ac0d21bc5fa30374c2aac7a4a2ffe72b8f95
sehouse <sehouse@umich.edu>
2020-08-07 16:58:45 -0400

    Main index.js written.

---
+++ src/components/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataArea from '../DataArea/index.js';
+
+const Main = () => {
+    return (
+        <>
+            <DataArea />
+        </>
+    );
+}
+
+export default Main;
